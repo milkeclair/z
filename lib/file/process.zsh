fbeff24d8870c72ea032e7e89971a7e39904c53f
milkeclair <milkeclair.black@gmail.com>
2025-10-26 20:03:08 +0900

    feat: [green] fileをちょっと拡張

--- lib/file/process.zsh
+++ lib/file/process.zsh
@@ -26,3 +26,91 @@ z.file.make_with_dir() {
   z.dir.make path=$(dirname $file)
   z.file.make path=$file
 }
+
+# write content to a file (overwrite)
+#
+# $path: file path
+# $content: content to write
+# REPLY: null
+# return: null
+#
+# example:
+#  z.file.write path="/path/to/file.txt" content="hello world"
+z.file.write() {
+  z.arg.named path $@ && local file=$REPLY
+  z.arg.named content $@ && local content=$REPLY
+
+  echo -n "$content" > "$file"
+}
+
+# append content to the end of a file
+#
+# $path: file path
+# $content: content to append
+# REPLY: null
+# return: null
+#
+# example:
+#  z.file.write.last path="/path/to/file.txt" content="hello world"
+z.file.write.last() {
+  z.arg.named path $@ && local file=$REPLY
+  z.arg.named content $@ && local content=$REPLY
+
+  echo "$content" >> "$file"
+}
+
+# read content from a file
+#
+# $path: file path
+# REPLY: file content
+# return: null
+#
+# example:
+#  z.file.read path="/path/to/file.txt"
+z.file.read() {
+  z.arg.named path $@ && local file=$REPLY
+
+  if z.file.exist $file; then
+    REPLY=$(cat "$file")
+  else
+    z.return
+  fi
+}
+
+# read lines from a file into an array
+#
+# $path: file path
+# REPLY: array of lines
+# return: null
+#
+# example:
+#  z.file.read.lines path="/path/to/file.txt"
+z.file.read.lines() {
+  z.arg.named path $@ && local file=$REPLY
+
+  if z.file.exist $file; then
+    REPLY=(${(@f)$(cat "$file")})
+  else
+    z.return
+  fi
+}
+
+# pick a line containing a specific word from a file
+#
+# $path: file path
+# $word: word to search for
+# REPLY: line containing the word|null
+# return: null
+#
+# example:
+#  z.file.read.pick path="/path/to/file.txt" word="search_word"
+z.file.read.pick() {
+  z.arg.named path $@ && local file=$REPLY
+  z.arg.named word $@ && local word=$REPLY
+
+  if z.file.exist $file; then
+    REPLY=$(grep -w "$word" "$file" | head -n 1)
+  else
+    z.return
+  fi
+}
