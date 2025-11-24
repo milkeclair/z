bc93e7d980f0cad98c71bccd32cb87c81ffe2524
milkeclair <milkeclair.black@gmail.com>
2025-11-24 19:28:18 +0900

    feat: [green] z.arr.sort

--- lib/arr/process.zsh
+++ lib/arr/process.zsh
@@ -11,3 +11,35 @@ z.arr.join() {
 
   z.return "${(j: :)arr}"
 }
+
+# sort array elements
+#
+# $by?: sort by (asc|desc), default: asc
+# $@: array elements
+# REPLY: sorted array elements
+# return: null
+#
+# example:
+#  z.arr.sort by=asc "b" "c" "a" #=> REPLY=("a" "b" "c")
+#  z.arr.sort by=desc "b" "c" "a" #=> REPLY=("c" "b" "a")
+z.arr.sort() {
+  z.arg.named by $@
+  local sort_by=${REPLY:-asc}
+  z.arg.named.shift by $@
+
+  local arr=($REPLY)
+
+  case $sort_by in
+  asc)
+    z.return $(printf "%s\n" "${arr[@]}" | sort)
+    ;;
+  desc)
+    z.return $(printf "%s\n" "${arr[@]}" | sort -r)
+    ;;
+  *)
+    z.return ${arr[@]}
+    z.io.error "z.arr.sort: invalid sort order: ${sort_by}"
+    return 1
+    ;;
+  esac
+}
