1dc5a9e951ae71586deefbe4c247bbdfa47ae070
milkeclair <milkeclair.black@gmail.com>
2025-11-30 20:15:29 +0900

    feat: [green] sets

--- lib/var/operator.zsh
+++ lib/var/operator.zsh
@@ -9,5 +9,19 @@
 #   z.var.exists TEST_VAR
 #   echo $?  # outputs 0
 z.var.exists() {
-  [[ -v $1 ]]
+  (( ${(P)+1} ))
+}
+
+# determine if a variable is settled (declared and assigned)
+#
+# $1: variable name
+# REPLY: null
+# return: 0\1
+#
+# example:
+#   local TEST_VAR="Hello, World!"
+#   z.var.sets TEST_VAR
+#   echo $?  # outputs 0
+z.var.sets() {
+  z.var.exists $1 && z.is_not_null ${(P)1}
 }
--- test/var/operator_test.zsh
+++ test/var/operator_test.zsh
@@ -1,6 +1,28 @@
 source ${z_main}
 
 z.t.describe "z.var.exists"; {
+  z.t.context "変数に値が存在する場合"; {
+    z.t.it "trueを返す"; {
+      local TEST_VAR_EXISTENT="Hello, World!"
+
+      z.var.exists TEST_VAR_EXISTENT
+
+      z.t.expect.status true
+      unset TEST_VAR_EXISTENT
+    }
+  }
+
+  z.t.context "変数が宣言されている場合"; {
+    z.t.it "trueを返す"; {
+      declare TEST_VAR_DECLARED
+
+      z.var.exists TEST_VAR_DECLARED
+
+      z.t.expect.status true
+      unset TEST_VAR_DECLARED
+    }
+  }
+
   z.t.context "変数が存在しない場合"; {
     z.t.it "falseを返す"; {
       unset TEST_VAR_NON_EXISTENT
@@ -10,14 +32,38 @@ z.t.describe "z.var.exists"; {
       z.t.expect.status false
     }
   }
+}
 
-  z.t.context "変数が存在する場合"; {
+z.t.describe "z.var.sets"; {
+  z.t.context "変数に値が存在する場合"; {
     z.t.it "trueを返す"; {
       local TEST_VAR_EXISTENT="Hello, World!"
 
-      z.var.exists TEST_VAR_EXISTENT
+      z.var.sets TEST_VAR_EXISTENT
 
       z.t.expect.status true
+      unset TEST_VAR_EXISTENT
+    }
+  }
+
+  z.t.context "変数が宣言されている場合"; {
+    z.t.it "falseを返す"; {
+      declare TEST_VAR_DECLARED
+
+      z.var.sets TEST_VAR_DECLARED
+
+      z.t.expect.status false
+      unset TEST_VAR_DECLARED
+    }
+  }
+
+  z.t.context "変数が存在しない場合"; {
+    z.t.it "falseを返す"; {
+      unset TEST_VAR_NON_EXISTENT
+
+      z.var.sets TEST_VAR_NON_EXISTENT
+
+      z.t.expect.status false
     }
   }
 }
