feb11f11a284513837026d41e8a173d6d6857260
milkeclair <milkeclair.black@gmail.com>
2026-02-22 12:43:17 +0900

    style: [green] スタイルを他に合わせる

--- mod/git/log/process.zsh
+++ mod/git/log/process.zsh
@@ -1,11 +1,11 @@
 z.git.log() {
   z.git.user.show
 
-  echo ""
-  echo "--- commits ---"
-  z.git.log._pretty
+  z.io.empty
+  z.io "--- commits ---"
+  z.git.log.pretty
 }
 
-z.git.log._pretty() {
+z.git.log.pretty() {
   command git log --graph --date=format:'%y/%m/%d %R' --pretty=format:' %C(yellow)%h%Creset [%C(green)%cd%Creset] %C(cyan)%cn%Creset%C(auto)%d%Creset%n %s%n%w(120,1,1)%b'
 }
