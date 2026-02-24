6eb5b20c53fb41ae649b5391cdfd26df569b2876
milkeclair <milkeclair.black@gmail.com>
2026-02-24 12:03:27 +0900

    feat: [green] pull

---
+++ mod/git/pull/process.zsh
@@ -0,0 +1,47 @@
+z.git.pull() {
+  z.arg.first $@ && local first_arg=$REPLY
+  z.is.eq $first_arg "pull" && shift
+
+  if z.git.hp.arg.has.origin "$@"; then
+    command git pull "$@"
+    return
+  fi
+
+  if z.git.hp.arg.has.develop "$@"; then
+    z.git.pull.develop
+    return
+  fi
+
+  z.git.pull.pr || z.git.pull.current
+}
+
+z.git.pull.develop() {
+  z.io "Pulling latest changes from develop branch"
+  z.io.empty
+  command git pull origin develop
+}
+
+z.git.pull.pr() {
+  z.git.branch.current
+  local current_branch=$REPLY
+
+  z.str.match.rest "$current_branch" "pr/"
+  local pr_number=$REPLY
+
+  if z.str.is.not.empty "$pr_number" && z.int.is.match "$pr_number"; then
+    z.io "Pulling latest changes for PR #${pr_number}"
+    z.io.empty
+    command git pull origin pull/"$pr_number"/head:"$current_branch"
+  else
+    return 1
+  fi
+}
+
+z.git.pull.current() {
+  z.git.branch.current
+  local current_branch=$REPLY
+
+  z.io "Pulling latest changes for current branch $current_branch"
+  z.io.empty
+  command git pull origin "$current_branch"
+}
