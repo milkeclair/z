83f4d19d0d3add5dc750a9b4164f1c07a23be78e
milkeclair <milkeclair.black@gmail.com>
2026-02-27 15:25:54 +0900

    feat: [green] branchを追加

---
+++ mod/git/branch/process.zsh
@@ -0,0 +1,23 @@
+# git branch alias
+#
+# $@: git branch options and arguments
+# REPLY: null
+# return: null
+#
+# example:
+#   z.git.branch -a # show all branches
+z.git.branch() {
+  command git branch "$@"
+}
+
+# alias for `git branch -a`
+#
+# $@: git branch options and arguments
+# REPLY: null
+# return: null
+#
+# example:
+#   z.git.branch.all # show all branches
+z.git.branch.all() {
+  z.git.branch -a
+}
