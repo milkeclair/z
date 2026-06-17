8d0c2bf86546713aa84b6a7f9f4ab36988ef8c52
milkeclair <milkeclair.black@gmail.com>
2026-06-18 03:52:15 +0900

    feat: [green] 定数名の変更

--- mod/wt_proxy/constant.zsh
+++ mod/wt_proxy/constant.zsh
@@ -4,16 +4,16 @@ typeset -g z_wt_proxy_config_dir_name=worktree-proxy
 typeset -g z_wt_proxy_config_file_extension=env
 typeset -g z_wt_proxy_port_range=1000
 typeset -g z_wt_proxy_default_slug=missing_slug
-typeset -g z_wt_proxy_public_port_env_prefix=Z_WT_PROXY_PUBLIC_PORT_
-typeset -g z_wt_proxy_public_port_config_prefix=public_port_
-typeset -g z_wt_proxy_port_key_prefix=port_
-typeset -g z_wt_proxy_host_port_env_prefix=Z_WT_PROXY_HOST_PORT_
+typeset -g z_wt_proxy_proxy_port_env_prefix=Z_WT_PROXY_PROXY_PORT_
+typeset -g z_wt_proxy_proxy_port_key_prefix=proxy_port_
+typeset -g z_wt_proxy_worktree_port_key_prefix=worktree_port_
+typeset -g z_wt_proxy_worktree_port_env_prefix=Z_WT_PROXY_WORKTREE_PORT_
 typeset -g z_wt_proxy_docker_compose_project_label=com.docker.compose.project
 
 typeset -gA z_wt_proxy_default_config_values=(
-  public_port_1 3000
-  public_port_2 5432
-  public_port_3 5173
+  proxy_port_1 3000
+  proxy_port_2 5432
+  proxy_port_3 5173
 )
 
 typeset -ga z_wt_proxy_localhost_hosts=(
