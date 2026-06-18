8d5565375befb09a8d1ce1ca54426f5fb2aa7f4f
milkeclair <milkeclair.black@gmail.com>
2026-06-19 05:50:21 +0900

    fix(wtproxy): preserve compose name hash suffix

--- mod/wtproxy/entry/compose/analysis.zsh
+++ mod/wtproxy/entry/compose/analysis.zsh
@@ -17,6 +17,23 @@ z.wtproxy._entry.compose.name() {
   local branch_hash=$(print -rn -- "$branch" | sha1sum)
   branch_hash=${branch_hash%% *}
 
-  local name="$config[project]_${branch_slug[1,42]}_${branch_hash[1,8]}"
-  z.return ${name[1,63]}
+  local hash_suffix=${branch_hash[1,8]}
+  local max_name_length=63
+  local separators_length=2
+  local parts_length=$((max_name_length - separators_length - ${#hash_suffix}))
+  local branch_part=${branch_slug[1,42]}
+  local project_part=$config[project]
+
+  if (( ${#project_part} + ${#branch_part} > parts_length )); then
+    local branch_length=${#branch_part}
+    if (( branch_length > parts_length )); then
+      branch_length=$parts_length
+    fi
+
+    local project_length=$((parts_length - branch_length))
+    project_part=${project_part[1,project_length]}
+    branch_part=${branch_part[1,branch_length]}
+  fi
+
+  z.return "${project_part}_${branch_part}_${hash_suffix}"
 }
--- test/mod/wtproxy/entry/compose/analysis_test.zsh
+++ test/mod/wtproxy/entry/compose/analysis_test.zsh
@@ -24,9 +24,49 @@ z.t.describe "z.wtproxy._entry.compose.name"; {
         z.return.hash config
       '
 
-      z.wtproxy._entry.compose.name "feat/very-long-branch-name-for-compose-project-name"
+      local branch="feat/very-long-branch-name-for-compose-project-name"
+      local branch_hash=$(print -rn -- "$branch" | sha1sum)
+      branch_hash=${branch_hash%% *}
+
+      z.wtproxy._entry.compose.name "$branch"
 
       z.t.expect "${#REPLY}" 63
+      z.t.expect "${REPLY[-8,-1]}" "${branch_hash[1,8]}"
+    }
+
+    z.t.it "project名が長くてもhash suffixを保持する"; {
+      z.t.mock name="z.wtproxy._config" behavior='
+        local -A config=(project extremely_long_project_name_that_would_otherwise_remove_hash_suffix)
+        z.return.hash config
+      '
+      local branch="feat/branch-a"
+      local branch_hash=$(print -rn -- "$branch" | sha1sum)
+      branch_hash=${branch_hash%% *}
+
+      z.wtproxy._entry.compose.name "$branch"
+
+      z.t.expect "${#REPLY}" 63
+      z.t.expect "${REPLY[-8,-1]}" "${branch_hash[1,8]}"
+    }
+
+    z.t.it "先頭が同じ長いbranchでも異なるcompose project名を返す"; {
+      z.t.mock name="z.wtproxy._config" behavior='
+        local -A config=(project very_long_project_name_for_compose)
+        z.return.hash config
+      '
+      local branch_a="feat/very-long-branch-name-for-compose-project-name-a"
+      local branch_b="feat/very-long-branch-name-for-compose-project-name-b"
+
+      z.wtproxy._entry.compose.name "$branch_a"
+      local name_a=$REPLY
+
+      z.wtproxy._entry.compose.name "$branch_b"
+      local name_b=$REPLY
+
+      z.is.not.eq "$name_a" "$name_b"
+      z.t.expect.status.is.true
+      z.t.expect "${#name_a}" 63
+      z.t.expect "${#name_b}" 63
     }
   }
 }
